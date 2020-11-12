98af5308b3e0a10bda2f3b0cd36dc67781d667de
Andreas Gillmeier <ag1110@students.necsd.net>
2020-11-12 15:11:19 +0000

    I finished I guess

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,17 @@
-
+import java.util.Scanner ;
 
 public class Chapter2Test {
   public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  System.out.println("please enter an integer ");
+  double firstNum = scan.nextDouble();
+  System.out.println("please enter another integer ");
+  double secondNum = scan.nextDouble();
+  System.out.println(firstNum);
+  System.out.println(secondNum);
+  double average = (firstNum + secondNum) / 2;
+  System.out.println("the average of the two numbers is "+ average);
+  
   
   }
 }
